00ad3e1827de56e8b23edce2750a448cf636c555
猫酱 <cathyye2000@gmail.com>
2020-02-21 17:42:37 +0800

    Create auro_eval.sh

---
+++ eval/auro_eval.sh
@@ -0,0 +1,4 @@
+for i in {1..50..1} #from 1 to 50 step 1
+do
+   #what you input in your command line eg.python tools/test.py work_dirs/fcos_r50_caffe_fpn_gn_1x_4gpu/epoch_${i}.pth --eval bbox 
+done
